7c32f93d28bb9882b497a2c558f39c6142ee4380
Nicolas Soroka <sorokanicolas@gmail.com>
2023-07-04 00:22:26 -0300

    tailwind applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
-import { GlobalProvider } from './context/GlobalState'
+import { GlobalProvider } from "./context/GlobalState";
 
-import Header from './components/Header'
-import Balance from './components/Balance'
-import TransactionForm from './components/transactions/TransactionForm'
-import TransactionList from './components/transactions/TransactionList'
-import IncomeExpenses from './components/IncomeExpenses'
+import Balance from "./components/Balance";
+import TransactionForm from "./components/transactions/TransactionForm";
+import TransactionList from "./components/transactions/TransactionList";
+import IncomeExpenses from "./components/IncomeExpenses";
 
 const App = () => {
   return (
     <GlobalProvider>
-      <Header />
-      <IncomeExpenses/>
-      <Balance/>
-      <TransactionForm/>
-      <TransactionList/>
+      <div className="bg-zinc-900 text-white h-screen flex justify-center items-center">
+        <div className="container mx-auto w-3/6 ">
+        <div className="bg-zinc-800 p-10 rounded-lg flex gap-x-5">
+          <div>
+            <h1 className="text-4xl font-bold">Expense Tracker</h1>
+            <IncomeExpenses />
+            <Balance />
+            <TransactionForm />
+          </div>
+          <div className="w-full">
+            <TransactionList />
+          </div>
+        </div>
+        </div>
+      </div>
     </GlobalProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/transactions/TransactionForm.jsx
+++ src/components/transactions/TransactionForm.jsx
@@ -1,32 +1,42 @@
-import { useState } from 'react'
-import { useGlobalState } from '../../context/GlobalState';
-
+import { useState } from "react";
+import { useGlobalState } from "../../context/GlobalState";
 
 const TransactionForm = () => {
   const { addTransaction } = useGlobalState();
-  const [description, setDescription] = useState('');
+  const [description, setDescription] = useState("");
   const [amount, setAmount] = useState(0);
-  
+
   const onSubmit = (e) => {
     e.preventDefault();
     addTransaction({
-      id : window.crypto.randomUUID(),
+      id: window.crypto.randomUUID(),
       description,
-      amount : +amount
-    })
-  }
+      amount: +amount,
+    });
+  };
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input onChange={(e) => setDescription(e.target.value)} type="text" placeholder='Enter a description'/>
-        <input onChange={(e) => setAmount(e.target.value)} type="number" step={0.5} placeholder='0.0'/>
-        <button>
+        <input
+          onChange={(e) => setDescription(e.target.value)}
+          type="text"
+          placeholder="Enter a description"
+          className="bg-zinc-600 text-white px-3 py-2 rounded-lg block mb-2 w-full"
+        />
+        <input
+          onChange={(e) => setAmount(e.target.value)}
+          type="number"
+          step={0.5}
+          placeholder="0.0"
+          className="bg-zinc-600 text-white px-3 py-2 rounded-lg block mb-2 w-full"
+        />
+        <button className="bg-indigo-700 text-white px-3 py-2 rounded-lg block mb-2 w-full ">
           Add transaction
         </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default TransactionForm
+export default TransactionForm;
